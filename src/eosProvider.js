3b064656af610d4a90308da895aea573ba3bbe73
bagaking <zfaustk@qq.com>
2018-11-12 16:29:03 +0800

    feat: definition of eosProvider

---
+++ src/eosProvider.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = class EOSProvider {
+
+    /**
+     * get or create scatter
+     * @return {eosAPI}
+     */
+    get eosClient() {
+        throw new Error(`method not yet implemented: this interface should be implement by the specific class.`)
+    }
+
+    /**
+     * getIdentity of cur scatter user
+     * @return {Promise<{Identity}>}
+     */
+    async getIdentity() {
+        throw new Error(`method not yet implemented: this interface should be implement by the specific class.`)
+        // it should be like that : '{ name: "nameofuser", authority: "active" }'
+    }
+
+}
