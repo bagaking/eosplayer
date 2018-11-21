73169eaf60047468ce0c0044d3995699e629b849
bagaking <zfaustk@qq.com>
2018-11-21 13:16:10 +0800

    feat (util): eventHandler - deal with the fallback on nodejs, remove unimplemented method 'async emit'

--- src/utils/eventHandler.js
+++ src/utils/eventHandler.js
@@ -2,6 +2,9 @@
 
 /**
  * Event Handler
+ * @desc
+ * - only enabled event can be emitted
+ * - If the callback of an event is not set, alert will be called by default on the browser, and Error will be triggered in nodejs
  * @author kinghand@foxmail.com
  */
 class EventHandler {
@@ -9,7 +12,12 @@ class EventHandler {
     constructor(supportedEvents) {
         this._eventMap = {};
         this._defaultCb = function (v) {
-            alert(`${JSON.stringify(this)} : ${JSON.stringify(v)}`);
+            let strTrigger = `${JSON.stringify(this)} : ${JSON.stringify(v)}`;
+            if(alert) {
+                alert(strTrigger);
+            }else{
+                throw new Error(strTrigger);
+            }
         }
 
         if (!!supportedEvents) {
@@ -18,18 +26,33 @@ class EventHandler {
         }
     }
 
+    /**
+     * enable event names
+     * @param {Array | Object} supportedEvents - keys will be used when it's an object
+     */
     enableEvents(supportedEvents) {
         let newEventKeys = Array.isArray(supportedEvents) ? supportedEvents : Object.keys(supportedEvents);
         this._supportedEvents = (this._supportedEvents || []).concat(newEventKeys);
     }
 
-    setEvent(event, fnCallback, context) {
+    /**
+     * set callback of an event name
+     * @param {string} event - event name
+     * @param {Function} fnCallback - if there is already a callback, then the new one will cover the previous one.
+     * @param {any} instance - the instance of the callback.
+     */
+    setEvent(event, fnCallback, instance) {
         if (!this._supportedEvents.find(name => name === event)) {
             throw new Error(`event handler : event ${event} are not supported.`);
         }
         this._eventMap[event] = {cb: fnCallback, ctx: context}
     }
 
+    /**
+     * trigger an event by name
+     * @param {string} event - event name
+     * @param {Array} args - arguments
+     */
     emitEvent(event, ...args) {
         if (!this._supportedEvents.find(name => name === event)) {
             throw new Error(`event handler : event ${event} are not found.`);
@@ -39,9 +62,6 @@ class EventHandler {
         return e ? e.cb.call(e.ctx, ...args) : this._defaultCb.call(event, ...args);
     }
 
-    async emitEventAsync(event, ...args) {
-        return await emitEvent(event, ...args);
-    }
 }
 
 module.exports = EventHandler;
