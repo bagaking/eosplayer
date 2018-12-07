c78fcdc2dc57ab2ff9077c1b221a1edcfbb68c1b
bagaking <zfaustk@qq.com>
2018-12-07 16:29:25 +0800

    feat (helper/kh): add new helper khRes

--- src/helpers/kh.js
+++ src/helpers/kh.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Asset = require("../utils/asset")
+const ResHelper = require("./khRes")
 const TranscalPayload = require("../utils/transcalPayload")
 
 /**
@@ -57,34 +57,56 @@ class KhHelper {
     }
 
     /**
+     * get res helper of (code, sym)
+     * @example kh.res('thecontract', 'WOD')
+     * @param code - the contract's account
+     * @param symStr - symbol of resource
+     */
+    res(code, symStr) {
+        return new ResHelper(this._chain, code, symStr);
+    }
+
+    /**
      * check res of an user
+     * @deprecated - using res(code, symStr).checkBalance(userAccount) instead
      * @param code - contract name
      * @param userAccount - account of the user
      * @param symStr - symbol string like "EOS"
      * @return {Promise<Asset>} - returns null if it's not exist.
      */
     async checkResOf(code, userAccount, symStr) {
-        return Asset.parse(await this._chain.checkTableItem(code, "res.accounts", userAccount, symStr));
+        return await this.res(code, symStr).checkBalance(userAccount);
     }
 
     /**
      * check res of an user
+     * @deprecated - using res(code, symStr).checkInfo() instead
      * @param code - contract name
      * @param symStr - symbol string like "EOS"
      * @return {Promise<Asset>} - returns null if it's not exist.
      */
     async checkResInfo(code, symStr) {
-        return Asset.parse(await this._chain.checkTableItem(code, "res.info", code, symStr));
+        return await this.res(code, symStr).checkInfo();
     }
 
 
     /**
-     * parse transcall payload to data structure
+     * parse transcal payload to data structure
      * @param memo
-     * @return {Promise<TranscalPayload>}
+     * @return {TranscalPayload}
      */
     parseTranscalPayload(memo) {
         return TranscalPayload.parse(memo);
+    }
+
+    /**
+     * assemble transcal data structure to payload
+     * @param func
+     * @param args
+     * @return {string}
+     */
+    assembleTranscalPayload(func, ...args) {
+        return (new TranscalPayload(func, ...args)).memo();
     }
 }
 
