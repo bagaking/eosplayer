db2914b1fafee4b15a1bd45a66168784c224b082
bagaking <zfaustk@qq.com>
2019-01-27 03:51:03 +0800

    feat : add readingPlayer.js

--- src/signPlayer.js
+++ src/signPlayer.js
@@ -7,7 +7,7 @@ const defaultConfig = {
     authority: 'active'
   },
   node: {
-    keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'],
+    keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79sample'],
     chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
     httpEndpoint: 'http://dev.toneos.pro:7777',
     mockTransactions: () => null,
